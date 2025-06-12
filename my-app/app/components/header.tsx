693359869324b19c3627b8767ccb9d605141d0ad
Yaarabi <yaarabi318@gmail.com>
2025-06-12 22:17:42 +0100

    finalisation of My portfolio

--- my-app/app/components/header.tsx
+++ my-app/app/components/header.tsx
@@ -4,8 +4,8 @@ import "../styles/header.css";
 const Header = () => {
 return (
     <nav className="nav">
-        <Link className="link" href="/">Home</Link>
-        <Link className="link" href="/about">About</Link>
+        <Link className="link" href="/">Who I'm</Link>
+        <Link className="link" href="/about">About me</Link>
         <Link className="link" href="/projects">My projects</Link>
         <Link className="link" href="/contact">Contact me</Link>
     </nav>
--- my-app/app/contact/page.tsx
+++ my-app/app/contact/page.tsx
@@ -1,12 +1,45 @@
 
-
+import Link from "next/link"
 
 
 const Index = () => {
     return (
-        <div>
-            hello from contact !
-        </div>
+        
+        <section className="">
+            <h2 className="text-4xl font-bold text-gray-900 mb-6">Contact Me</h2>
+            <p className="text-lg text-gray-700 mb-6">
+                Feel free to reach out for collaborations, job opportunities, or discussions related to web development and e-commerce strategies!
+            </p>
+
+            {/* Contact Methods */}
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                <div className="bg-gray-100 p-6 rounded-lg shadow-md text-center">
+                <h3 className="text-2xl font-semibold text-gray-800">Email</h3>
+                <p className="text-gray-700 mt-3">
+                    📧 <a href="mailto:yaarabi@gmail.com" className="text-blue-600 hover:underline">yaarabi@gmail.com</a>
+                </p>
+            </div>
+
+                <div className="bg-gray-100 p-6 rounded-lg shadow-md text-center">
+                    <h3 className="text-2xl font-semibold text-gray-800">LinkedIn</h3>
+                    <p className="text-gray-700 mt-3">
+                    🔗 <Link href="https://www.linkedin.com/in/youssef-aarabi-519b55237/" target="_blank">
+                    <span className="text-blue-600 hover:underline">Visit my LinkedIn</span>
+                    </Link>
+                    </p>
+                </div>
+
+                <div className="bg-gray-100 p-6 rounded-lg shadow-md text-center">
+                    <h3 className="text-2xl font-semibold text-gray-800">GitHub</h3>
+                    <p className="text-gray-700 mt-3">
+                    💻 <Link href="https://github.com/Yaarabi" target="_blank">
+                    <span className="text-blue-600 hover:underline">Check out my projects</span>
+                    </Link>
+                    </p>
+                </div>
+            </div>
+    </section>
+
 )
 }
 
