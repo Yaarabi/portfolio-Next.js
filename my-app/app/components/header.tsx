37eff7370e3a417f9ab0fe7d8d9815452569060e
Yaarabi <yaarabi318@gmail.com>
2025-06-12 22:32:31 +0100

    change the metadata of the project

--- my-app/app/components/header.tsx
+++ my-app/app/components/header.tsx
@@ -4,7 +4,7 @@ import "../styles/header.css";
 const Header = () => {
 return (
     <nav className="nav">
-        <Link className="link" href="/">Who I'm</Link>
+        <Link className="link" href="/">Who I`m</Link>
         <Link className="link" href="/about">About me</Link>
         <Link className="link" href="/projects">My projects</Link>
         <Link className="link" href="/contact">Contact me</Link>
